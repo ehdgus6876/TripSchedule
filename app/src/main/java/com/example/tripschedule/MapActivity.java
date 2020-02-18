f13beb347f8aaecc20e06c73eab97b081793d4b4
qjatn1009 <a43727702@gmail.com>
2020-02-18 22:30:35 +0900

    commit

---
+++ app/src/main/java/com/example/tripschedule/MapActivity.java
@@ -0,0 +1,34 @@
+package com.example.tripschedule;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        mapView=findViewById(R.id.map_view);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+        marker.setMap(naverMap);
+
+    }
+}
--- app/src/main/java/com/example/tripschedule/SelectBasket.java
+++ app/src/main/java/com/example/tripschedule/SelectBasket.java
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectBasket extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
     private SelectAdapter adapter;
+    private Button btn_select,btn_map;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_basket);
+        btn_select=findViewById(R.id.btn_select);
+        btn_map=findViewById(R.id.btn_map);
 
         recyclerView=findViewById(R.id.rv_speedBus);
         layoutManager=new LinearLayoutManager(this);
@@ -31,6 +37,15 @@ public class SelectBasket extends AppCompatActivity {
         adapter=new SelectAdapter();
         adapter.notifyDataSetChanged();
         recyclerView.setAdapter(adapter);
+
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MapActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 
     }
