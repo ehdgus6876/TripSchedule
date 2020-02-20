1a2ed2b8c98711cdd5fb1c43cf09f8d783413d99
qjatn1009 <a43727702@gmail.com>
2020-02-20 20:36:21 +0900

    commit

--- app/src/main/java/com/example/tripschedule/MapActivity.java
+++ app/src/main/java/com/example/tripschedule/MapActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.naver.maps.geometry.LatLng;
+import com.naver.maps.geometry.Utmk;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
@@ -27,7 +28,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         Marker marker = new Marker();
-        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+        //marker.setPosition(new LatLng(Float.valueOf(FoodAdapter.selectItems.get(0).getMapx()),Float.valueOf(FoodAdapter.selectItems.get(0).getMapy())));
+        Utmk utmk = new Utmk(Float.valueOf(FoodAdapter.selectItems.get(0).getMapx()),Float.valueOf(FoodAdapter.selectItems.get(0).getMapy()));
+        LatLng latLng = utmk.toLatLng();
+        marker.setPosition(latLng);
         marker.setMap(naverMap);
 
     }
